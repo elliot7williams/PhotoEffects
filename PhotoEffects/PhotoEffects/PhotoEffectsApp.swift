ff8125efc2e199b41f4f70080d9bd2f77617ac32
elliot7williams <58864955+elliot7williams@users.noreply.github.com>
2026-08-01 22:00:43 -0400

    Add PhotoEffects/PhotoEffects/PhotoEffectsApp.swift

---
+++ PhotoEffects/PhotoEffects/PhotoEffectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoEffectsApp.swift
+//  PhotoEffects
+//
+//  Created by Elliot Williams on 2025-07-09.
+//
+
+import SwiftUI
+
+@main
+struct PhotoEffectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
