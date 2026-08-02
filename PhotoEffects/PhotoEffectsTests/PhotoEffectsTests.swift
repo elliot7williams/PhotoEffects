3cdf85bf0995ac81226c1643aa76da0bb6331889
elliot7williams <58864955+elliot7williams@users.noreply.github.com>
2026-08-01 22:00:35 -0400

    Add PhotoEffects/PhotoEffectsTests/PhotoEffectsTests.swift

---
+++ PhotoEffects/PhotoEffectsTests/PhotoEffectsTests.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoEffectsTests.swift
+//  PhotoEffectsTests
+//
+//  Created by Elliot Williams on 2025-07-09.
+//
+
+import Testing
+@testable import PhotoEffects
+
+struct PhotoEffectsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
